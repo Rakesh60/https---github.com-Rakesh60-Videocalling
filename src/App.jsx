90c53830599631ef33541574547e31c55422b1f6
Rakesh60 <rakeshhupta5353@gmail.com>
2024-01-08 16:06:37 +0530

    app created

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+} from "react-router-dom";
+
+import Home from "./pages/home/home"
+import Room from "./pages/room/room";
+
+function App() {
+  return (
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/room/:roomId" element={<Room />} />
+        </Routes>
+      </Router>
+    </div>
+  )
+}
+
+export default App
