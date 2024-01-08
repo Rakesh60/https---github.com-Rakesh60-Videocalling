9cd25ef2ad3f058f632308b2b42a4fa744a49eaf
Rakesh60 <rakeshhupta5353@gmail.com>
2024-01-08 16:19:45 +0530

    link changed

--- src/pages/room/room.jsx
+++ src/pages/room/room.jsx
@@ -23,7 +23,7 @@ export default function Room() {
             container: element,
             sharedLinks: [{
                 name: 'Copy Link',
-                url: `http://localhost:5173/room/${roomId}`
+                url: `https://https-github-com-rakesh60-videocalling.vercel.app/room/${roomId}`
             }],
             scenario: {
                 mode: ZegoUIKitPrebuilt.OneONoneCall
